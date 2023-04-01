30b1b7007bcf64da9d04a273fc58e573d7e40266
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-04-01 14:25:28 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -9,7 +10,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!inputText) return; // add this line to prevent empty input
-  
+
     try {
       const response = await fetch("/generate-content", {
         method: "POST",
@@ -24,10 +25,36 @@ function App() {
       console.error(error);
     }
   };
-  
+  const [pdfFile, setPdfFile] = useState(null);
+
+  // Handle file input change
+  const handleFileChange = (event) => {
+    setPdfFile(event.target.files[0]);
+  };
+
+  // Handle file upload
+  const handleFileUpload = () => {
+    const formData = new FormData();
+    formData.append("pdfFile", pdfFile);
+
+    axios
+      .post("/upload", formData)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <div className="App">
+      <div>
+        <h1>PDF File Uploader</h1>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleFileUpload}>Upload</button>
+      </div>
+
       <h1>Generate Content</h1>
       <form onSubmit={handleSubmit}>
         <label>
--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+
+const multer = require("multer");
+const pdf = require("pdf-parse");
+
 const sdk = require("api")("@writesonic/v2.2#4enbxztlcbti48j");
 
 const apiKey = "ecb4490a-cd1e-475e-b53b-e23db844690c";
@@ -15,16 +19,49 @@ sdk.auth(apiKey);
 app.post("/generate-content", async (req, res) => {
   const { inputText } = req.body;
   try {
-    const response = await sdk.chatsonic_V2BusinessContentChatsonic_post({
-      enable_google_results: true,
-      enable_memory: false,
-      input_text: inputText,
-    }, {engine: 'premium'});
+    const response = await sdk.chatsonic_V2BusinessContentChatsonic_post(
+      {
+        enable_google_results: true,
+        enable_memory: false,
+        input_text: inputText,
+      },
+      { engine: "premium" }
+    );
     res.json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
+});
+
+// Set storage engine for multer
+const storage = multer.memoryStorage();
+
+// Init upload
+const upload = multer({
+  storage: storage,
+}).single("pdfFile");
+
+// Route for uploading PDF file
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      // Parse the PDF file and console log the contents
+      const pdfData = req.file.buffer;
+      pdf(pdfData)
+        .then((data) => {
+          console.log("HELLO ......     ",data.text);
+          res.sendStatus(200);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+    }
+  });
 });
 
 app.listen(port, () => {

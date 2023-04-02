393500a4ca361d1e1a46328062c681f5414be17b
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-04-02 13:38:30 +0530

    added loaders

---
+++ client/src/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./loading.css"
+import Gif from "./load.gif"
+function Loading() {
+  return (
+    <div className="centered">
+    <img src={Gif} alt="Example Gif" className="gif" />
+    <h1>Loading ...</h1>
+  </div>
+  );
+}
+
+export default Loading;

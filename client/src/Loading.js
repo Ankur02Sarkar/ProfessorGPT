7308567acae5ca783618dcac27c6f00f61854521
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-04-03 00:55:53 +0530

    deployed

--- client/src/Loading.js
+++ client/src/Loading.js
@@ -4,7 +4,7 @@ import Gif from "./load.gif"
 function Loading() {
   return (
     <div className="centered">
-    <img src={Gif} alt="Example Gif" className="gif" />
+    <img src="https://s2.gifyu.com/images/load.md.gif" alt="Loading Gif" className="gif" />
     <h1 className="loadHeading">Loading ...</h1>
   </div>
   );
